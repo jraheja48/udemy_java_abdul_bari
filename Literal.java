5f233e229ea6d3672df9f29c4aa7b1a46ddd3d29
jraheja48 <jraheja48@gmail.com>
2023-05-28 21:31:12 -0500

    updating .gitignore

--- Literal.java
+++ Literal.java
@@ -2,7 +2,6 @@ import java.lang.*;
 
 class Literal {
 	public static void main(String myArgs[]) {
-		/*
 		byte b1 = 10;
 		byte b2 = 0b1010;
 		byte b3 = 012;
@@ -14,7 +13,7 @@ class Literal {
 		System.out.println(b3);
 		System.out.println(b4);
 		System.out.println(l);
-		*/
+		
 
 		int sum  = Integer.sum(5, 15);
 		System.out.println(sum);		
